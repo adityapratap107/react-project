f027111c2c8af6bf0b0c5f4b001e73170fc5004e
Aditya Rana <ranaaditya104@gmail.com>
2023-07-23 23:30:19 +0530

    Simple JSX code

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return(
+        <div class="expense-item">
+            <div>July 22 2023</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.23</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
