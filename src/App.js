bb22c849c544ea10c3759084e68d833960193641
Aditya Pratap Singh <aditya.singh@netsolutions.com>
2023-07-24 21:15:53 +0530

    passing data via props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {id: 1, title: "Car Insurance", price: 294.23, date: new Date(2023, 7, 22)},
+    {id: 2, title: "Desktop", price: 100.23, date: new Date(2023, 6, 18)},
+    {id: 3, title: "Bike", price: 300.23, date: new Date(2023, 6, 12)},
+    {id: 4, title: "Car", price: 2000.23, date: new Date(2023, 5, 7)},
+  ];
   return (
     <div className="App">
-          <ExpenseItem/>
-          <p>Let's Start!</p>
+      <h2>Let's Start!</h2>
+      
+      {/* MULTIPLE PROPS WAY */}
+      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].price} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].price} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].price} date={expenses[2].date} />
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].price} date={expenses[3].date} /> */}
+
+      {/* SINGLE PROP WAY */}
+      {/* <ExpenseItem expense={expenses[0]} />
+      <ExpenseItem expense={expenses[1]} />
+      <ExpenseItem expense={expenses[2]} />
+      <ExpenseItem expense={expenses[3]} /> */}
+
+      {/* OBJECT DESTRUCTURING WAY */}
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].price} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].price} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].price} date={expenses[2].date} />
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].price} date={expenses[3].date} />
 
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,51 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    return(
-        <div class="expense-item">
-            <div>July 22 2023</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.23</div>
-            </div>
-        </div>
-    );
+// function ExpenseItem({title, date, amount}) {
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
+
+  return (
+    // <div className="expense-item">
+    //     <div className=''>{props.date.toISOString()}</div>
+    //     <div className='expense-description'>
+    //         <h2>{props.title}</h2>
+    //         <div className='expense-price'>${props.amount}</div>
+    //     </div>
+    // </div>
+
+    // Alternative way
+    // <div className="expense-item">
+    //     <div className=''>{props.expense.date.toISOString()}</div>
+    //     <div className='expense-description'>
+    //         <h2>{props.expense.title}</h2>
+    //         <div className='expense-price'>${props.expense.price}</div>
+    //     </div>
+    // </div>
+
+    // OBJECT DESTRUCTURING WAY
+    // <div className="expense-item">
+    //   <div className="">{date.toISOString()}</div>
+    //   <div className="expense-description">
+    //     <h2>{title}</h2>
+    //     <div className="expense-price">${amount}</div>
+    //   </div>
+    // </div>
+
+    // Adding normal javascript logic to component
+    <div className="expense-item">
+      <div className="">
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-description">
+        <h2>{props.title}</h2>
+        <div className="expense-price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
