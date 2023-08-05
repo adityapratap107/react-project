4b2c3a68b9012e2320c0b5a18ec95767e3e1993c
Aditya Pratap Singh <aditya.singh@netsolutions.com>
2023-08-05 22:48:39 +0530

    state compleye

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseItem from '../ExpenseItem/ExpenseItem'
+import ExpensesFilter from '../ExpensesFilter';
 
 const Expenses = ({items}) => {
   // console.log('Expenses Items', props.items)
+
+  const [filteredYear, setFilteredYear] = useState('2023');
+  // const [filterInfoText, setFilterInfoText] = useState('2021, 2022 & 2024')
+
+  let filterInfoText = '2021, 2022 & 2024'
+   if(filteredYear === '2021') {
+      filterInfoText = '2022, 2023 & 2024'
+    } else if (filteredYear === '2022') {
+      filterInfoText = '2021, 2023 & 2024'
+    } else if (filteredYear === '2023') {
+      filterInfoText = '2021, 2022 & 2024'
+    } else if (filteredYear === '2024') {
+      filterInfoText = '2021, 2022 & 2023'
+    } 
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+    // if(selectedYear === '2021') {
+    //   setFilterInfoText('2022, 2023 & 2024')
+    // } else if (selectedYear === '2022') {
+    //   setFilterInfoText('2021, 2023 & 2024')
+    // } else if (selectedYear === '2023') {
+    //   setFilterInfoText('2021, 2022 & 2024')
+    // } else if (selectedYear === '2024') {
+    //   setFilterInfoText('2021, 2022 & 2023')
+    // } 
+  };
+
+
+
   return (
     <div>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <p>Data for years {filterInfoText} is hidden.</p>
         {/* MULTIPLE PROPS WAY */}
       <ExpenseItem title={items[0].title} amount={items[0].price} date={items[0].date} />
       <ExpenseItem title={items[1].title} amount={items[1].price} date={items[1].date} />
